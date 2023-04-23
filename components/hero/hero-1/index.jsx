26d36aebf9ef11dc79d0100049a95ebaaced4b19
Ovi13-max <129941475+Ovi13-max@users.noreply.github.com>
2023-04-23 14:26:06 +0300

    Update index.jsx

--- components/hero/hero-1/index.jsx
+++ components/hero/hero-1/index.jsx
@@ -14,7 +14,7 @@ const index = () => {
                 className="text-60 lg:text-40 md:text-30 text-white"
                 data-aos="fade-up"
               >
-                Te iubesc Tania!
+                Cautati urmatoarea dvs locatie !!
               </h1>
               <p
                 className="text-white mt-6 md:mt-10"
